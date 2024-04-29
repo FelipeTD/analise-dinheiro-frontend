f000d2b5cf680335babbd7fb5b97fca800e39ce7
filipe.dias <fedispato@gmail.com>
2024-04-28 22:08:20 -0300

    feat: Login finalizado para um login simples. Precisa saber como fazer o consumo de um JWT agora

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,14 +1,80 @@
+import { useState } from "react";
+import axios from 'axios';
+
 function Login() {
-    return (
-        <div className="login-form-wrap">
-        <h2>Login</h2>
-        <form className="login-form">
-            <input type="email" name="email" placeholder="Email" required />
-            <input type="password" name="password" placeholder="Senha" required />
-            <button type="submit" className="btn-login" >Login</button>
-        </form>
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [user, setUser] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:6060/auth/signin",
+        JSON.stringify({ email, password }),
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+
+      if (response.data.statusCode === 200) {
+        setUser(response.data);
+      } else if (response.data.statusCode === 500) {
+        setError("Usuário ou senha inválidos");
+      }
+
+    } catch (error) {
+      if (!error?.response) {
+        setError("Erro ao acessar o servidor");
+      }
+    }
+
+  }
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    setUser("");
+    setError("");
+  }
+
+  return (
+    <div className="login-form-wrap">
+      {user === "" ? (
+        <div>
+          <h2>Login</h2>
+          <form className="login-form">
+            <input type="email"
+              name="email"
+              placeholder="Email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input type="password"
+              name="password"
+              placeholder="Senha"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit"
+              className="btn-login"
+              onClick={(e) => handleLogin(e)}
+            >Login</button>
+          </form>
+          <p>{error}</p>
         </div>
-    );
+      ) : (
+        <div>
+          <h2>{user.message}</h2>
+          <button type="button" 
+                  className="btn-login"
+                  onClick={(e) => handleLogout(e)}
+          >
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Login;
